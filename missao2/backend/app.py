42ffa1a61beae40d66a639879772f6a9cd4f125f
Juan Fricke <juanpfricke@outlook.com>
2024-09-23 21:11:41 -0300

    Changes to backend

--- missao2/backend/app.py
+++ missao2/backend/app.py
@@ -1,11 +1,23 @@
-# backend/app.py
 from flask import Flask, jsonify
+import os
+import json
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return jsonify({"message": "Backend Flask is running!"})
+# Defina o caminho para a pasta 'json' e o arquivo 'classes.json'
+json_folder = os.path.join(os.getcwd(), 'json')
+json_file = os.path.join(json_folder, 'classes.json')
+
+@app.route('/get_classes', methods=['GET'])
+def get_classes():
+    try:
+        # Abrir o arquivo JSON e carregá-lo
+        with open(json_file, 'r') as file:
+            data = json.load(file)
+        # Retornar o conteúdo como JSON
+        return jsonify(data), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True)
