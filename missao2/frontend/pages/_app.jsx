d2ad7744a6bae1ab5c7c9a50b98488eca0435980
GabrielaZanetti <gabiisctt@gmail.com>
2024-09-23 21:01:55 -0300

    inclusao do modal de participar de uma turma

--- missao2/frontend/pages/_app.jsx
+++ missao2/frontend/pages/_app.jsx
@@ -3,15 +3,19 @@ import { useState } from "react";
 import "../styles/global.css";
 import Menu from "../components/Menu";
 import MenuLateral from "../components/MenuLateral";
+import Modal from "../components/Modal";
 
 export default function App() {
   const [open, setOpen] = useState(false);
-  const turmas = []
+  const [openModal, setOpenModal] = useState(false);
+  const turmas = [];
+console.log(openModal);
 
   return (
     <>
-      <Menu menuOpen={setOpen} open={open} />
+      <Menu menuOpen={setOpen} open={open} setOpenModal={setOpenModal} />
       <MenuLateral open={open} turmas={turmas} />
+      {openModal && <Modal setOpenModal={setOpenModal} />}
     </>
   );
 }
