c52f7412d84ad0193a4c4bb6363bb67964b72fe8
Juan Fricke <juanpfricke@outlook.com>
2024-09-25 19:51:19 -0300

    colors

--- missao2/frontend/components/Card/index.jsx
+++ missao2/frontend/components/Card/index.jsx
@@ -5,10 +5,11 @@ export default function Card({
   descricao = '',
   fotoProfessor = 'https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png', // Imagem do professor
   urlImg = 'https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png', // Imagem da matéria (foto de capa)  corIcon = '#000' // Cor do ícone
+  corIcon = "#1976d2"
 }) {
   return (
     <div className={styles.cardContainer}>
-      <div className={styles.cardHeader}>
+      <div className={styles.cardHeader} style={{ backgroundColor: corIcon }} >
         <h2 className={styles.cardTitle}>{nome}</h2> {/* Nome da matéria */}
         <p className={styles.cardDescription}>{descricao}</p> {/* Nome do professor */}
         <img className={styles.cardImage} src={fotoProfessor} alt="Foto do professor" /> {/* Foto do professor */}
